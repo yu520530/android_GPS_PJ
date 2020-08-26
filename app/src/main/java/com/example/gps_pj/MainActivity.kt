2a6dbda7fd5202d8af74af5f59ec8a2b6f67440a
yu520530 <ya90312@gmail.com>
2020-08-26 22:41:30 +0800

    將原本的gps畫面刪除 改用內建activity 完成初始畫面

--- app/src/main/java/com/example/gps_pj/MainActivity.kt
+++ app/src/main/java/com/example/gps_pj/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btn_enter.setOnClickListener {
-            val intent = Intent(this,ActivityGpsLayout::class.java)
+           val intent = Intent(this,MapsActivity::class.java)
             startActivity(intent)
         }
 
