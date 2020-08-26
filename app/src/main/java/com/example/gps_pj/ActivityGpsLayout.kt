88f6e13928868c45b72bf64ae1abe7ec5ab2b4b1
yu520530 <ya90312@gmail.com>
2020-08-26 10:22:20 +0800

    "主畫面"新增按鈕 新增GPS畫面

---
+++ app/src/main/java/com/example/gps_pj/ActivityGpsLayout.kt
@@ -0,0 +1,11 @@
+package com.example.gps_pj
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityGpsLayout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gps_layout)
+    }
+}
