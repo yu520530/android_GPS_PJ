d09068bb79f4f77e5d657ac71945bc03beba7a6c
yu520530 <ya90312@gmail.com>
2020-08-29 23:23:34 +0800

    bulid.grade增加 apply plugin: 'com.google.gms.google-services', apply plugin: 'com.google.gms.google-services' 新增"MainActivity"登入功能

--- app/src/main/java/com/example/gps_pj/MainActivity.kt
+++ app/src/main/java/com/example/gps_pj/MainActivity.kt
@@ -3,15 +3,45 @@ package com.example.gps_pj
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    //將firebaseauth引入
+    private lateinit var auth:FirebaseAuth
+    //tag方便偵錯
+    var TAG:String = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btn_enter.setOnClickListener {
-           val intent = Intent(this,MapsActivity::class.java)
-            startActivity(intent)
+        //auth 取得值
+        FirebaseApp.initializeApp(this);
+        auth = FirebaseAuth.getInstance()
+
+        btn_login.setOnClickListener{
+            val email = ed_email.text.toString()
+            val psd = ed_psd.text.toString()
+
+            auth.signInWithEmailAndPassword(email,psd)
+                .addOnCompleteListener(this) {task ->
+                    if(task.isSuccessful)
+                    {
+                        Log.d(TAG,"login success")
+                        val intent = Intent(this,MapsActivity::class.java)
+                        Toast.makeText(this,"login success",Toast.LENGTH_SHORT).show()
+                        startActivity(intent)
+                    }
+                    else
+                    {
+                        Log.e(TAG,"login unsuccess",task.exception)
+                        Toast.makeText(this,"please check your email and password",Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+
         }
 
     }
