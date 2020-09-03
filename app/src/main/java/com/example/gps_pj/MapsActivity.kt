ccded586ec1929fbb35701d92767f1922c0c381f
yu520530 <ya90312@gmail.com>
2020-09-03 23:18:40 +0800

    新增"MapsActivity"同意使用gps功能

--- app/src/main/java/com/example/gps_pj/MapsActivity.kt
+++ app/src/main/java/com/example/gps_pj/MapsActivity.kt
@@ -10,6 +10,7 @@ import android.os.Bundle
 import androidx.core.app.ActivityCompat
 
 import android.location.LocationListener
+import android.os.Build
 import android.util.Log
 import android.widget.Toast
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -18,19 +19,29 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     var orilocation : Location? = null
     val TAG = "MapsActivity"
+    private  val REQUEST_PERMISSIONS = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_PERMISSIONS)
+        else{
+            val mapFragment = supportFragmentManager
+                .findFragmentById(R.id.map) as SupportMapFragment
+            mapFragment.getMapAsync(this)
+        }
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -55,9 +66,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     fun  drawMarker()
     {
         var lntLng = LatLng(orilocation!!.latitude, orilocation!!.longitude)
-        mMap.addMarker(MarkerOptions().position(lntLng).title("I'm here!"))
+        mMap.addMarker(MarkerOptions().position(lntLng).title(getNowTimeDetail()))
         Toast.makeText(this, "改變位置", Toast.LENGTH_LONG).show()
     }
+
 
     private fun locationManager()
     {
@@ -74,13 +86,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 if(isGPSEnable)
                 {
                     //註冊 LocationManager 要向哪個服務取得位置更新資訊
-                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000L,20f,locationListener)
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,5000L,10f,locationListener)
                     //取得上一次的定位
                     orilocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
                 }
                 else if(isNETWORKEnable)
                 {
-                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,5000L,20f,locationListener)
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,5000L,10f,locationListener)
                     orilocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
                 }
             } catch (ex:SecurityException)
@@ -100,13 +112,40 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             {
                 orilocation = location
                 drawMarker()
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(location.latitude, location.longitude), 12.0f))
             }
             else
             {
                 orilocation = location
                 drawMarker()
             }
-            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(LatLng(location.latitude, location.longitude), 12.0f))
+
+        }
+    }
+    //取得時間
+    fun getNowTimeDetail(): String? {
+        val sdf = SimpleDateFormat("HH時mm分ss秒")
+        return sdf.format(Date())
+    }
+    //同意使用再app上使用gps
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(grantResults.isEmpty()) return
+        when(requestCode){
+            REQUEST_PERMISSIONS -> {
+                for (result in grantResults)
+                    if(result != PackageManager.PERMISSION_GRANTED)
+//                        finish()
+                    else {
+                        val map = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+
+                        map.getMapAsync(this)
+                    }
+            }
         }
     }
 }
